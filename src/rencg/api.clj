f9a84db43fbf09ae7989a9514b87ad50cc1b4d47
Peter Monks <pmonks@gmail.com>
2023-07-23 12:03:52 -0700

    :new: Adds re-find-ncg (issue #7)

--- src/rencg/api.clj
+++ src/rencg/api.clj
@@ -18,40 +18,87 @@
 
 (ns rencg.api)
 
-(defn re-named-groups
+(defmulti re-named-groups
   "Returns the names of all of the named-capturing groups in the
-  given regular expression, as a set of Strings.
+  given regular expression (or matcher), as a set of Strings.
 
   Note: JDK-agnostic workaround for https://bugs.openjdk.org/browse/JDK-7032377
   (which is fixed in JDK 20)"
-  [re]
-  (when re (set (map second (re-seq #"\(\?<([a-zA-Z][a-zA-Z0-9]*)>" (str re))))))
+  {:arglists '([re] [m])}
+  (fn [arg] (type arg)))
 
+(defmethod re-named-groups nil
+  [_]
+  nil)
+
+(defmethod re-named-groups java.util.regex.Pattern
+  [^java.util.regex.Pattern re]
+  (set (map second (re-seq #"\(\?<([a-zA-Z][a-zA-Z0-9]*)>" (str re)))))
+
+(defmethod re-named-groups java.util.regex.Matcher
+  [^java.util.regex.Matcher m]
+  (re-named-groups (.pattern m)))
+
+(defn re-groups-ncg
+  "Equivalent to clojure.core/re-groups, but instead of returning
+  a sequence containing the entire match and each group, it returns
+  a (potentially empty) map of just the named-capturing groups. Each
+  key in the map is the String value of the name of that named-
+  capturing group, and the corresponding value is a String containing
+  the text that matched that group."
+  ([^java.util.regex.Matcher m] (re-groups-ncg m nil))
+  ([^java.util.regex.Matcher m ncgs]
+   (let [ncgs (or ncgs (re-named-groups m))]
+     (loop [result {}
+            f      (first ncgs)
+            r      (rest ncgs)]
+       (if f
+         (let [v (try (.group m ^String f) (catch java.lang.IllegalArgumentException _ nil))]
+           (recur (merge result (when v {f v}))
+                  (first r)
+                  (rest r)))
+         result)))))
 
 (defn re-matches-ncg
-  "Equivalent to clojure.core/re-matches, but instead of returning
-  the match and sequence of groups, returns a (potentially empty)
-  map of just the named-capturing groups in the regex if there was
-  a match, or nil if the regex didn't match. Each key in the map is
-  the String value of the name of that named-capturing group, and
-  the corresponding value is a String containing the text that
-  matched that group.
+  "Equivalent to clojure.core/re-matches, but returns the result
+  of calling re-groups-ncg when there's a match, or nil otherwise.
 
   If the regex is being reused many times, the 3-arg version will
   be more efficient as it allows the caller to calculate the
   named-capturing groups in the regex once, then reuse that
   information, avoiding re-parsing of the regex on each call."
-  ([re s] (re-matches-ncg re s nil))
-  ([re s ncgs]
-   (let [matcher (re-matcher re s)]
-     (when (.matches matcher)
-       (let [ncgs (or ncgs (re-named-groups re))]
-         (loop [result {}
-                f      (first ncgs)
-                r      (rest ncgs)]
-           (if f
-             (let [v (try (.group matcher ^String f) (catch java.lang.IllegalArgumentException _ nil))]
-               (recur (merge result (when v {f v}))
-                      (first r)
-                      (rest r)))
-             result)))))))
+  ([^java.util.regex.Pattern re s] (re-matches-ncg re s nil))
+  ([^java.util.regex.Pattern re s ncgs]
+   (let [m (re-matcher re s)]
+     (when (.matches m)
+       (re-groups-ncg m ncgs)))))
+
+(defmulti re-find-ncg
+  "Equivalent to clojure.core/re-find, but returns the result
+  of calling re-groups-ncg when the pattern is found, or nil
+  otherwise.
+
+  If multiple finds are being performed, the versions where
+  the sequence of named-capturing groups is passed in will be
+  more efficient as they allow the caller to calculate the
+  named-capturing groups in the regex once, then reuse that
+  information, avoiding re-parsing of the regex on each call."
+  {:arglists '([m] [m ncgs] [re s] [re s ncgs])}
+  (fn [f & _] (type f)))
+
+(defmethod re-find-ncg nil
+  [& _]
+  (re-find nil))  ; This call to re-find may seem bogus, however it ensures we throw _exactly_ the same exception that it throws when passed nil
+
+(defmethod re-find-ncg java.util.regex.Matcher
+  ([^java.util.regex.Matcher m] (re-find-ncg m nil))
+  ([^java.util.regex.Matcher m ncgs]
+   (when (.find m)
+     (re-groups-ncg m ncgs))))
+
+(defmethod re-find-ncg java.util.regex.Pattern
+  ([^java.util.regex.Pattern re s] (re-find-ncg re s nil))
+  ([^java.util.regex.Pattern re s ncgs]
+   (let [m (re-matcher re s)]
+     (re-find-ncg m ncgs))))
+
--- test/rencg/api_test.clj
+++ test/rencg/api_test.clj
@@ -18,7 +18,7 @@
 
 (ns rencg.api-test
   (:require [clojure.test :refer [deftest testing is]]
-            [rencg.api    :refer [re-named-groups re-matches-ncg]]))
+            [rencg.api    :refer [re-named-groups re-matches-ncg re-find-ncg]]))
 
 (def apache-re #"(?i)(?<name>Apache)(\s+Software)?(\s+License(s)?(\s*[,-])?)?(\s+V(ersion)?)?\s*(?<version>\d+(\.\d+)?)?")
 
@@ -64,7 +64,7 @@
     (is (= {"name" "Apache", "version" "2.0"} (re-matches-ncg apache-re         "Apache 2.0")))
     (is (= {"name" "Apache", "version" "1"}   (re-matches-ncg apache-re         "Apache 1")))
     (is (= {"name" "Apache", "version" "2"}   (re-matches-ncg apache-re         "Apache Software License Version 2"))))
-  (testing "3-arg version"
+  (testing "Matches with pre-computed ncgs"
     (let [ncgs (re-named-groups apache-re)]
       ; Note: these cases are nonsensical since the names in ncgs don't correlate to the regexes, but we test these cases anyway to ensure reasonable behaviour
       (is (nil?                                 (re-matches-ncg #"foo"         ""                                  ncgs)))
@@ -78,3 +78,65 @@
       (is (= {"name" "Apache", "version" "2.0"} (re-matches-ncg apache-re      "Apache 2.0"                        ncgs)))
       (is (= {"name" "Apache", "version" "1"}   (re-matches-ncg apache-re      "Apache 1"                          ncgs)))
       (is (= {"name" "Apache", "version" "2"}   (re-matches-ncg apache-re      "Apache Software License Version 2" ncgs))))))
+
+(deftest re-find-ncg-tests
+  (testing "Nil regexes and/or input strings"
+    ; Not a fan of throwing exceptions in these cases, but for better or worse this behaviour is compatible with clojure.core/re-matches
+    (is (thrown? java.lang.NullPointerException (re-find-ncg nil   nil)))
+    (is (thrown? java.lang.NullPointerException (re-find-ncg #".*" nil)))
+    (is (thrown? java.lang.NullPointerException (re-find-ncg nil   ""))))
+  (testing "Non-finds that don't have named-capturing groups"
+    (is (nil? (re-find-ncg #"foo"   "")))
+    (is (nil? (re-find-ncg #"foo"   "bar")))
+    (is (nil? (re-find-ncg #"(foo)" ""))))
+  (testing "Non-finds that do have named-capturing groups"
+    (is (nil? (re-find-ncg #"(?<foo>foo)" "")))
+    (is (nil? (re-find-ncg apache-re      "Mozilla"))))
+  (testing "Finds that don't have named-capturing groups"
+    (is (= {} (re-find-ncg #".*"  "")))
+    (is (= {} (re-find-ncg #"foo" "foo"))))
+  (testing "Finds that do have named-capturing groups, but they don't have values in the matched text"
+    (is (= {} (re-find-ncg #"(?<foo>foo)?.*" "bar"))))
+  (testing "Finds that do have named-capturing groups, and some or all of them have values"
+    (is (= {"foo" "foo"}                      (re-find-ncg #"(?<foo>foo)"    "foo")))
+    (is (= {"foo" "foo"}                      (re-find-ncg #"(?<foo>foo)"    "prefix foo suffix")))
+    (is (= {"content" "foobar"}               (re-find-ncg #"(?<content>.*)" "foobar")))
+    (is (= {"name" "Apache"}                  (re-find-ncg apache-re         "Apache")))
+    (is (= {"name" "apache"}                  (re-find-ncg apache-re         "apache")))
+    (is (= {"name" "Apache", "version" "2.0"} (re-find-ncg apache-re         "Apache 2.0")))
+    (is (= {"name" "Apache", "version" "1"}   (re-find-ncg apache-re         "Apache 1")))
+    (is (= {"name" "Apache", "version" "2"}   (re-find-ncg apache-re         "Apache Software License Version 2")))
+    (is (= {"name" "Apache", "version" "2"}   (re-find-ncg apache-re         "prefix Apache Software License Version 2 suffix"))))
+  (testing "Repeated finds, reusing the same matcher"
+    (let [re   #"(?<foo>foo)"
+          s    "foofoofoo"
+          ncgs (re-named-groups re)
+          m    (re-matcher re s)]
+      (is (= {"foo" "foo"} (re-find-ncg m ncgs)))   ; First foo in s
+      (is (= {"foo" "foo"} (re-find-ncg m ncgs)))   ; Second foo in s
+      (is (= {"foo" "foo"} (re-find-ncg m ncgs)))   ; Third foo
+      (is (nil?            (re-find-ncg m ncgs))))  ; No more foos in s
+    (let [re   #"(?<foo>foo)"
+          s    "prefix foo interstitial text foo suffix"
+          ncgs (re-named-groups re)
+          m    (re-matcher re s)]
+      (is (= {"foo" "foo"} (re-find-ncg m ncgs)))    ; First foo in s
+      (is (= {"foo" "foo"} (re-find-ncg m ncgs)))    ; Second foo in s
+      (is (nil?            (re-find-ncg m ncgs)))))  ; No more foos in s
+  (testing "Finds with pre-computed ncgs"
+    (let [ncgs (re-named-groups apache-re)]
+      ; Note: these cases are nonsensical since the names in ncgs don't correlate to the regexes, but we test these cases anyway to ensure reasonable behaviour
+      (is (nil?                                 (re-find-ncg #"foo"         ""                                                ncgs)))
+      (is (nil?                                 (re-find-ncg #"(?<foo>foo)" ""                                                ncgs)))
+      (is (nil?                                 (re-find-ncg #"(?<foo>foo)" "bar"                                             ncgs)))
+      (is (= {}                                 (re-find-ncg #"foo"         "foo"                                             ncgs)))
+      (is (= {}                                 (re-find-ncg #"foo"         "prefix foo suffix"                               ncgs)))
+      ; These cases make more sense
+      (is (= {"foo" "foo"}                      (re-find-ncg #"(?<foo>foo)" "foo"                                             #{"foo"})))
+      (is (nil?                                 (re-find-ncg apache-re      "Mozilla"                                         ncgs)))
+      (is (= {"name" "Apache"}                  (re-find-ncg apache-re      "Apache"                                          ncgs)))
+      (is (= {"name" "apache"}                  (re-find-ncg apache-re      "apache"                                          ncgs)))
+      (is (= {"name" "Apache", "version" "2.0"} (re-find-ncg apache-re      "Apache 2.0"                                      ncgs)))
+      (is (= {"name" "Apache", "version" "1"}   (re-find-ncg apache-re      "Apache 1"                                        ncgs)))
+      (is (= {"name" "Apache", "version" "2"}   (re-find-ncg apache-re      "Apache Software License Version 2"               ncgs)))
+      (is (= {"name" "Apache", "version" "2"}   (re-find-ncg apache-re      "prefix Apache Software License Version 2 suffix" ncgs))))))
